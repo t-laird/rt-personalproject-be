d1ba0c09a3cba3696896b2aa0fd10e9f6fdf7d01
Thomas Laird <t6r6l5@gmail.com>
2017-12-21 11:39:30 -0700

    commented out other seeds while working on users

--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -1,39 +1,39 @@
-let transactionData = [
-  {
-    'send_id': 123,
-    'receive_id': 456,
-    'group_id': 789,
-    'point_value': 5},
-  {
-    'send_id': 123,
-    'receive_id': 456,
-    'group_id': 789,
-    'point_value': 5}
-];
+// let transactionData = [
+//   {
+//     'send_id': 123,
+//     'receive_id': 456,
+//     'group_id': 789,
+//     'point_value': 5},
+//   {
+//     'send_id': 123,
+//     'receive_id': 456,
+//     'group_id': 789,
+//     'point_value': 5}
+// ];
 
-const createEvent = (knex, event) => {
-  return knex('eventtracking').insert({
-    send_id: event.send_id,
-    receive_id: event.receive_id,
-    group_id: event.group_id,
-    point_value: event.point_value
-  }, 'event_id');
-};
+// const createEvent = (knex, event) => {
+//   return knex('eventtracking').insert({
+//     send_id: event.send_id,
+//     receive_id: event.receive_id,
+//     group_id: event.group_id,
+//     point_value: event.point_value
+//   }, 'event_id');
+// };
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('eventtracking').del()
-    .then(() => {
-      let eventPromises = [];
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('eventtracking').del()
+//     .then(() => {
+//       let eventPromises = [];
 
-      transactionData.forEach(event => {
-        eventPromises.push(createEvent(knex, event));
-      });
+//       transactionData.forEach(event => {
+//         eventPromises.push(createEvent(knex, event));
+//       });
 
-      return Promise.all(eventPromises);
-    })
-    .catch(error => console.log(`error seeding data: ${error}`))
-};
+//       return Promise.all(eventPromises);
+//     })
+//     .catch(error => console.log(`error seeding data: ${error}`))
+// };
 
 let userData = [
   {
@@ -72,41 +72,41 @@ exports.seed = function(knex, Promise) {
     .catch(error => console.log(`error seeding data: ${error}`))
 };
 
-let groupData = [
-  {
-    group_name: 'turing',
-    group_passphrase: '9139z',
-    weekly_points: 100,
-    administrator_id: 123,
-  },
-  {
-    group_name: 'facebook',
-    group_passphrase: '19vns',
-    weekly_points: 1000,
-    administrator_id: 957,
-  }
-];
+// let groupData = [
+//   {
+//     group_name: 'turing',
+//     group_passphrase: '9139z',
+//     weekly_points: 100,
+//     administrator_id: 123,
+//   },
+//   {
+//     group_name: 'facebook',
+//     group_passphrase: '19vns',
+//     weekly_points: 1000,
+//     administrator_id: 957,
+//   }
+// ];
 
-const createGroup = ( knex, group ) => {
-  return knex('group').insert({
-    group_name: group.name,
-    group_passphrase: group.group_passphrase,
-    weekly_points: group.weekly_points,
-    administrator_id: group.administrator_id
-  }, 'group_id');
-};
+// const createGroup = ( knex, group ) => {
+//   return knex('group').insert({
+//     group_name: group.name,
+//     group_passphrase: group.group_passphrase,
+//     weekly_points: group.weekly_points,
+//     administrator_id: group.administrator_id
+//   }, 'group_id');
+// };
 
-exports.seed = function(knex, Promise) {
-  return knex('group').del()
-    .then(() => {
-      let groupPromises = [];
-      groupData.forEach(group => {
-        groupPromises.push(createGroup(knex, group));
-      });
+// exports.seed = function(knex, Promise) {
+//   return knex('group').del()
+//     .then(() => {
+//       let groupPromises = [];
+//       groupData.forEach(group => {
+//         groupPromises.push(createGroup(knex, group));
+//       });
 
-      return Promise.all(groupPromises);
-    })
-    .catch(error => console.log(`error seeding data: ${error}`))
-};
+//       return Promise.all(groupPromises);
+//     })
+//     .catch(error => console.log(`error seeding data: ${error}`))
+// };
 
 

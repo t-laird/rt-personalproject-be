45ff12948455e7f0e3e354a658d05f2a9db31766
Thomas Laird <t6r6l5@gmail.com>
2017-12-25 20:08:53 -0700

    more detailed and relevant seed files for server feature testing

---
+++ db/seeds/dev/groups.js
@@ -0,0 +1,48 @@
+let groupData = [
+  {
+    group_name: 'turing',
+    group_passphrase: '9139z',
+    weekly_points: 100,
+    administrator_id: 123,
+  },
+  {
+    group_name: 'facebook',
+    group_passphrase: '19vns',
+    weekly_points: 1000,
+    administrator_id: 957,
+  },
+  {
+    group_name: 'twitter',
+    group_passphrase: '19cix',
+    weekly_points: 100,
+    administrator_id: 957,
+  },
+  {
+    group_name: 'google',
+    group_passphrase: '31uza',
+    weekly_points: 100,
+    administrator_id: 957,
+  },
+];                                    
+
+const createGroup = ( knex, group ) => {
+  return knex('group').insert({
+    group_name: group.name,
+    group_passphrase: group.group_passphrase,
+    weekly_points: group.weekly_points,
+    administrator_id: group.administrator_id
+  }, 'group_id');
+};
+
+exports.seed = function(knex, Promise) {
+  return knex('group').del()
+    .then(() => {
+      let groupPromises = [];
+      groupData.forEach(group => {
+        groupPromises.push(createGroup(knex, group));
+      });
+
+      return Promise.all(groupPromises);
+    })
+    .catch(error => console.log(`error seeding data: ${error}`))
+};
--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -1,171 +1,67 @@
-const weeks = 
-  { one:   1514162103004,
-    two:   1512868287190,
-    three: 1512263487190,
-    four:  1511658687190,
-    five:  1511053887190 };
-
-let transactionData = [
+let userData = [
   {
-    'send_id': 1,
-    'receive_id': 5,
-    'group_id': 789,
-    'created_time': weeks.one, 
-    'point_value': 10},
+    group_id: 3,
+    email:'sjf@gmail.com',
+    name: 'Bob Smith',
+    authrocket_id: "ajwoc1k3j342"
+  },
   {
-    'send_id': 1,
-    'receive_id': 5,
-    'group_id': 789,
-    'created_time': weeks.two,
-    'point_value': 5},
+    group_id: 4,
+    email: 'abcf@gmail.com',
+    name: 'Mike Jenkins',
+    authrocket_id: "jfnxcio42393"
+  },
   {
-    'send_id': 1,
-    'receive_id': 7,
-    'group_id': 789,
-    'created_time': weeks.two,    
-    'point_value': 15},
+    group_id: 4,
+    email: 'qwi123@gmail.com',
+    name: 'Josh Brown',
+    authrocket_id: "lksdfjn23413"
+  },
   {
-    'send_id': 2,
-    'receive_id': 1,
-    'group_id': 789,
-    'created_time': weeks.three,    
-    'point_value': 20},
+    group_id: 4,
+    email: '293xcnv@gmail.com',
+    name: 'Alan Michaelson',
+    authrocket_id: "xmcv3941nv"
+  },
   {
-    'send_id': 2,
-    'receive_id': 3,
-    'group_id': 789,
-    'created_time': weeks.four,    
-    'point_value': 5},
+    group_id: 4,
+    email: 'nc-1n23a@gmail.com',
+    name: 'Sarah Hilman',
+    authrocket_id: "3490xxa1j"
+  },
   {
-    'send_id': 1,
-    'receive_id': 5,
-    'group_id': 789,
-    'created_time': weeks.one,   
-    'point_value': 15},
+    group_id: 3,
+    email: 'xmhhh@gmail.com',
+    name: 'Jane Johnson',
+    authrocket_id: "123jlkaf"
+  },
   {
-    'send_id': 1,
-    'receive_id': 3,
-    'group_id': 789,
-    'created_time': weeks.five,    
-    'point_value': 10},
-  {
-    'send_id': 2,
-    'receive_id': 1,
-    'group_id': 789,
-    'created_time': weeks.one,
-    
-    'point_value': 10},
-  {
-    'send_id': 4,
-    'receive_id': 3,
-    'group_id': 789,
-    'created_time': weeks.three,    
-    'point_value': 25},
-  {
-    'send_id': 5,
-    'receive_id': 3,
-    'group_id': 789,
-    'created_time': weeks.one,    
-    'point_value': 20}
+    group_id: 3,
+    email: 'bnzx9123@gmail.com',
+    name: 'Ally Benson',
+    authrocket_id: "zxqweio933"
+  }
 ];
 
-const createEvent = (knex, event) => {
-  return knex('eventtracking').insert({
-    send_id: event.send_id,
-    receive_id: event.receive_id,
-    group_id: event.group_id,
-    created_time: event.created_time,
-    point_value: event.point_value
-  }, 'event_id');
+const createUser = ( knex, user ) => {
+  return knex('users').insert({
+    group_id: user.group_id,
+    email: user.email, 
+    name: user.name,
+    authrocket_id: user.authrocket_id
+  }, 'user_id');
 };
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('eventtracking').del()
+  return knex('users').del()
     .then(() => {
-      let eventPromises = [];
-
-      transactionData.forEach(event => {
-        eventPromises.push(createEvent(knex, event));
+      let userPromises = [];
+      userData.forEach(user => {
+        userPromises.push(createUser(knex, user));
       });
 
-      return Promise.all(eventPromises);
+      return Promise.all(userPromises);
     })
     .catch(error => console.log(`error seeding data: ${error}`))
 };
-
-// let userData = [
-//   {
-//     group_id: 3,
-//     email:'sjf@gmail.com',
-//     name: 'bob',
-//     authrocket_id: 1
-//   },
-//   {
-//     group_id: 4,
-//     email: 'abcf@gmail.com',
-//     name: 'mike',
-//     authrocket_id: 3
-//   }
-// ];
-
-// const createUser = ( knex, user ) => {
-//   return knex('users').insert({
-//     group_id: user.group_id,
-//     email: user.email, 
-//     name: user.name,
-//     authrocket_id: user.authrocket_id
-//   }, 'user_id');
-// };
-
-// exports.seed = function(knex, Promise) {
-//   return knex('users').del()
-//     .then(() => {
-//       let userPromises = [];
-//       userData.forEach(user => {
-//         userPromises.push(createUser(knex, user));
-//       });
-
-//       return Promise.all(userPromises);
-//     })
-//     .catch(error => console.log(`error seeding data: ${error}`))
-// };
-
-// let groupData = [
-//   {
-//     group_name: 'turing',
-//     group_passphrase: '9139z',
-//     weekly_points: 100,
-//     administrator_id: 123,
-//   },
-//   {
-//     group_name: 'facebook',
-//     group_passphrase: '19vns',
-//     weekly_points: 1000,
-//     administrator_id: 957,
-//   }
-// ];
-
-// const createGroup = ( knex, group ) => {
-//   return knex('group').insert({
-//     group_name: group.name,
-//     group_passphrase: group.group_passphrase,
-//     weekly_points: group.weekly_points,
-//     administrator_id: group.administrator_id
-//   }, 'group_id');
-// };
-
-// exports.seed = function(knex, Promise) {
-//   return knex('group').del()
-//     .then(() => {
-//       let groupPromises = [];
-//       groupData.forEach(group => {
-//         groupPromises.push(createGroup(knex, group));
-//       });
-
-//       return Promise.all(groupPromises);
-//     })
-//     .catch(error => console.log(`error seeding data: ${error}`))
-// };
-
 

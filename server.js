481221daf8f1bf9aec6804656406b8e39b94933b
Thomas Laird <t6r6l5@gmail.com>
2018-01-10 10:31:06 -0700

    add-fix-all-origins

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
-  allowedOrigins: ['localhost:3001', 't-laird.github.io', 'localhost:3000', 't-laird.com', 'http://t-laird.com', 'https://t-laird.com'],
+  allowedOrigins: ['localhost:3001', 't-laird.github.io', 'localhost:3000', 't-laird.com', 'http://t-laird.com', 'https://t-laird.com', '*'],
   preflightContinue: true,
   headers: ['Content-Type', 'x-token']
 };

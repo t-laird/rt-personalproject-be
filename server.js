cdb9fa0abbad15d91623c9920b787996ff9c7600
Thomas Laird <t6r6l5@gmail.com>
2018-01-10 11:12:13 -0700

    allow x-token header

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const corsOptions = {
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://t-laird.com');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With x-token');
 
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {

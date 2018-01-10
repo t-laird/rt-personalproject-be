81f1a4b6f12f8563879d6f6971b72e15f366766b
Thomas Laird <t6r6l5@gmail.com>
2018-01-10 11:18:41 -0700

    try wildcard

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const corsOptions = {
 };
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://t-laird.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With x-token');
 

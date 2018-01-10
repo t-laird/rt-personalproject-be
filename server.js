bfb6ebc585497fc40261405c0b2abdc58a0eb170
Thomas Laird <t6r6l5@gmail.com>
2018-01-10 12:12:07 -0700

    h

--- server.js
+++ server.js
@@ -29,12 +29,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With x-token');
 
   // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
+  next();
 };
 
 app.use(allowCrossDomain);
